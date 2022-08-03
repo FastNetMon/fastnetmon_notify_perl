0ef984082fb69a9176a150d421db2ea6b7692163
Pavel Odintsov <pavel.odintsov@gmail.com>
2022-08-03 17:29:30 +0100

    Removed unprintable symbols

--- notify_json.pl
+++ notify_json.pl
@@ -13,8 +13,8 @@ open my $fl, ">>", "/tmp/fastnetmon_notify_script.log" or die "Could not open fi
 # This script executed from FastNetMon this way: ban 11.22.33.44
 
 if (scalar @ARGV != 2) {
-    print {$fl} "Please specify all arguments. Got only: @ARGV\n";
-    die "Please specify all arguments\n";
+    print {$fl} "Please specify all arguments. Got only: @ARGV\n";
+    die "Please specify all arguments\n";
 }
 
 my ($action, $ip_address) = @ARGV;
@@ -24,12 +24,12 @@ my ($action, $ip_address) = @ARGV;
 my $input_attack_details = join '', <STDIN>;
 
 # try to decode this data to json
-my $attack_details = eval{  decode_json($input_attack_details); };
+my $attack_details = eval{ decode_json($input_attack_details); };
 
 # report error
 if ($@) {
-    print {$fl} "JSON decode failed: $input_attack_details\n";
-    die "JSON decode failed\n";
+    print {$fl} "JSON decode failed: $input_attack_details\n";
+    die "JSON decode failed\n";
 }
 
 print {$fl} "Received notification about $ip_address with action $action\n";
